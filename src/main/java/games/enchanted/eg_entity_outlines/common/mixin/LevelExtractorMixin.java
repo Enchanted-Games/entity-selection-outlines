b4e9edbfda2fab98783da3df85958da5bb15d43e
ioblackshaw <isaacoblackshaw@gmail.com>
2026-07-07 19:23:52 +0100

    interpolate entity outline position

--- src/main/java/games/enchanted/eg_entity_outlines/common/mixin/LevelExtractorMixin.java
+++ src/main/java/games/enchanted/eg_entity_outlines/common/mixin/LevelExtractorMixin.java
@@ -8,8 +8,10 @@ import net.minecraft.client.renderer.extract.LevelExtractor;
 import net.minecraft.client.renderer.state.level.BlockOutlineRenderState;
 import net.minecraft.client.renderer.state.level.LevelRenderState;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.EntityHitResult;
+import net.minecraft.world.phys.Vec3;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import org.objectweb.asm.Opcodes;
@@ -31,11 +33,18 @@ public class LevelExtractorMixin {
     private void eg_entity_outlines$extractEntityOutlineState(LevelRenderState instance, BlockOutlineRenderState value, Operation<Void> original, Camera camera, LevelRenderState levelRenderState) {
         original.call(instance, value);
 
-        if (!(this.minecraft.hitResult instanceof EntityHitResult entityHitResult)) return;
+        if(!(this.minecraft.hitResult instanceof EntityHitResult entityHitResult)) return;
+        if(this.minecraft.level == null) return;
 
-        BlockPos entityBlockPos = entityHitResult.getEntity().blockPosition();
+        Entity entity = entityHitResult.getEntity();
+        BlockPos entityBlockPos = entity.blockPosition();
         boolean highContrast = this.minecraft.options.highContrastBlockOutline().get();
-        AABB bb = entityHitResult.getEntity().getBoundingBox();
+        float partialTicks = this.minecraft.getDeltaTracker().getGameTimeDeltaPartialTick(!this.minecraft.level.tickRateManager().isEntityFrozen(entity));
+
+        Vec3 latestPosition = entity.position();
+        Vec3 currentPosition = entity.getPosition(partialTicks);
+        Vec3 offset = currentPosition.subtract(latestPosition);
+        AABB bb = entity.getBoundingBox().move(offset);
         VoxelShape shape = Shapes.box(
             bb.minX - entityBlockPos.getX(),
             bb.minY - entityBlockPos.getY(),
