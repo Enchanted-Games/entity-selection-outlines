d25dd996519fd426171fe9b2f526b9da0c1d5260
ioblackshaw <isaacoblackshaw@gmail.com>
2026-07-08 17:39:54 +0100

    update config screen, add all toggles to it

--- src/main/java/games/enchanted/eg_entity_outlines/common/config/ConfigScreen.java
+++ src/main/java/games/enchanted/eg_entity_outlines/common/config/ConfigScreen.java
@@ -4,15 +4,33 @@ import games.enchanted.eg_entity_outlines.common.ModConstants;
 import games.enchanted.eg_entity_outlines.common.config.option.ConfigOption;
 import net.minecraft.client.gui.GuiGraphicsExtractor;
 import net.minecraft.client.gui.TextAlignment;
+import net.minecraft.client.gui.components.AbstractWidget;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.components.Tooltip;
+import net.minecraft.client.gui.layouts.GridLayout;
+import net.minecraft.client.gui.layouts.HeaderAndFooterLayout;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.CommonComponents;
 import net.minecraft.network.chat.Component;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.List;
+
 public class ConfigScreen extends Screen {
+    private static final List<ConfigOption<Boolean>> TOGGLES = List.of(
+        ConfigOptions.MOD_ENABLED_OPTION,
+        ConfigOptions.OUTLINE_BLOCK_ATTACHED,
+        ConfigOptions.OUTLINE_ARMOUR_STANDS,
+        ConfigOptions.OUTLINE_BOATS,
+        ConfigOptions.OUTLINE_MINECARTS,
+        ConfigOptions.OUTLINE_END_CRYSTALS,
+        ConfigOptions.OUTLINE_EVERYTHING
+    );
+
     private final Screen parentScreen;
+
+    private HeaderAndFooterLayout layout;
+    private GridLayout grid;
 
     ConfigScreen(Screen parent) {
         super(Component.translatableWithFallback("%s.configScreen.title".formatted(ModConstants.MOD_ID), ModConstants.MOD_NAME + " Config"));
@@ -21,40 +39,76 @@ public class ConfigScreen extends Screen {
 
     @Override
     protected void init() {
-        // mod enabled button
-        addRenderableWidget(startBooleanValueButton(ConfigOptions.MOD_ENABLED_OPTION)
-            .bounds(width / 2 - 100, height / 2 - 20, 200, 20)
-        .build());
+        this.layout = new HeaderAndFooterLayout(this, HeaderAndFooterLayout.DEFAULT_HEADER_AND_FOOTER_HEIGHT, HeaderAndFooterLayout.DEFAULT_HEADER_AND_FOOTER_HEIGHT);
+        this.grid = new GridLayout(0, this.layout.getHeaderHeight());
+        this.layout.addToContents(this.grid);
 
-        // done and cancel buttons
-        addRenderableWidget(
+        // mod enabled button
+        for (int i = 0; i < TOGGLES.size(); i++) {
+            ConfigOption<Boolean> option = TOGGLES.get(i);
+            int column = i % 2;
+            int row = i - column;
+
+            this.grid.addChild(
+                makeBooleanValueButton(option)
+                    .bounds((this.width / 2) - (Button.DEFAULT_WIDTH / 2), (this.height / 2), Button.DEFAULT_WIDTH, Button.DEFAULT_HEIGHT)
+                .build(),
+                row,
+                column
+            );
+        }
+
+        this.grid.newCellSettings().alignHorizontallyCenter();
+        this.grid.columnSpacing(6);
+        this.grid.rowSpacing(4);
+        this.grid.arrangeElements();
+        this.grid.visitWidgets(this::addRenderableWidget);
+
+        // done button
+        AbstractWidget doneButton = (
             Button.builder(
                 CommonComponents.GUI_DONE,
                 button -> onClose()
             )
-            .bounds(width / 2 - 75, height - 40, 150, 20)
+            .bounds((this.width / 2) - (Button.DEFAULT_WIDTH / 2), this.height - 40, Button.DEFAULT_WIDTH, Button.DEFAULT_HEIGHT)
         .build());
+
+        this.layout.addToFooter(doneButton);
+        this.addRenderableWidget(doneButton);
+
+        this.layout.arrangeElements();
     }
 
     @Override
     public void extractRenderState(@NotNull GuiGraphicsExtractor context, int mouseX, int mouseY, float delta) {
         super.extractRenderState(context, mouseX, mouseY, delta);
-        context.textRenderer().accept(TextAlignment.CENTER, width / 2, 30, this.title);
+        context.textRenderer().accept(TextAlignment.CENTER, this.width / 2, this.layout.getHeaderHeight(), this.title);
     }
 
     @Override
     public void onClose() {
         saveConfigOptions();
-        minecraft.gui.setScreen(parentScreen);
+        //? if minecraft: >= 26.2 {
+        this.minecraft.gui.setScreen(this.parentScreen);
+        //? } else {
+        /*this.minecraft.setScreen(this.parentScreen);
+        *///? }
+    }
+
+    @Override
+    protected void repositionElements() {
+        super.repositionElements();
+        this.layout.arrangeElements();
+        this.grid.arrangeElements();
     }
 
     private void saveConfigOptions() {
         ConfigOptions.saveIfAnyDirtyOptions();
     }
 
-    private Button.Builder startBooleanValueButton(ConfigOption<Boolean> value) {
+    private Button.Builder makeBooleanValueButton(ConfigOption<Boolean> value) {
         String configKey = value.getJsonKey();
-        Component buttonContent = Component.translatable(getTranslationKeyForOption(configKey));
+        Component buttonContent = Component.translatableWithFallback(getTranslationKeyForOption(configKey), configKey);
         Component buttonTooltipText = Component.translatable(getTranslationKeyForOption(configKey) + ".tooltip");
 
         return Button.builder(CommonComponents.optionNameValue(buttonContent, trueFalseOptionStatus(value.getPendingOrCurrentValue())),
